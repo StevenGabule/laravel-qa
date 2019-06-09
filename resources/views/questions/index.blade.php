ffa4bc748a314a9c9bb2b477f05f17016a14a240
John Doe <lucas.gabule@gmail.com>
2019-06-10 05:12:02 +0800

    implementing the delte button

--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -32,6 +32,13 @@
                                     <h3 class="mt-0"><a href="{{ $question->url }}">{{ $question->title }}</a></h3>
                                     <div class="ml-auto">
                                         <a href="{{ route('questions.edit', $question->id) }}" class="btn btn-sm btn-outline-info">Edit</a>
+                                        <form action="{{ route('questions.destroy', $question->id)}}" class="d-inline" method="post">
+                                            @method('DELETE')
+                                            @csrf
+                                            <button type="submit" class="btn btn-sm btn-outline-danger" onclick="return confirm('Are you sure to delete this question?')">
+                                                Delete
+                                            </button>
+                                        </form>
                                     </div>
                                 </div>
                                 <p class="lead">Asked by <a href="{{ $question->user->url }}">{{ $question->user->name }}</a>

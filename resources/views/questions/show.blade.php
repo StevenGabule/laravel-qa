77782d71fee6a5d7bfc45a9bb07756c8953b0220
John Doe <lucas.gabule@gmail.com>
2019-06-10 10:39:33 +0800

    add vote controls to question and answers

--- resources/views/questions/show.blade.php
+++ resources/views/questions/show.blade.php
@@ -5,41 +5,81 @@
     <div class="row justify-content-center">
         <div class="col-md-12">
             <div class="card">
-                <div class="card-header">
-                    <div class="d-flex align-items-center">
-                        <h2>{{ $question->title}}</h2>   
-                        <div class="ml-auto">
-                            <a href="{{ route('questions.index') }}" class="btn btn-outline-secondary">Back to all questions</a>
-                        </div>
-                    </div>
-                </div><!-- end of card header -->
+                
                 <div class="card-body">
-                    {!! $question->body_html !!}
-                    <div class="float-right">
-                        <span class="text-muted">Answered {{ $question->created_date }}</span>
-                        <div class="media mt-2">
-                            <a href="{{ $question->user->avatar }}" class="pr-2">
-                                <img src="{{ $question->user->avatar }}" alt="">
-                            </a>
-                            <div class="media-body mt-1">
-                            <a href="{{ $question->user->url }}">{{ $question->user->name }}</a>
+                    <div class="card-title">
+                        <div class="d-flex align-items-center">
+                            <h2>{{ $question->title}}</h2>   
+                            <div class="ml-auto">
+                                <a href="{{ route('questions.index') }}" class="btn btn-outline-secondary">Back to all questions</a>
                             </div>
                         </div>
-                    </div><!-- end of float-right -->
-                </div><!-- end of card-body -->
+                    </div><!-- end of card header -->
+                    
+                    <hr>
+
+                    <div class="media">
+                        <div class="d-flex flex-column vote-controls">
+                            <a title="This question is useful" class="vote-up">
+                                <i class="fas fa-caret-up fa-3x"></i>
+                            </a>
+                            <span class="votes-count">1230</span>
+                            <a title="This question is not useful" class="vote-down off">
+                                    <i class="fas fa-caret-down fa-3x"></i>
+                            </a>
+                            <a title="Click to mark as favorite question (Click again to undo)" class="favorite mt-2 favorited">
+                                <i class="fas fa-star fa-3x"></i>
+                                <span class="favorites-count">123</span>    
+                            </a>   
+                        </div><!-- end of d flex -->
+
+                        <div class="media-body">
+                            {!! $question->body_html !!}
+                            <div class="float-right">
+                                <span class="text-muted">Answered {{ $question->created_date }}</span>
+                                <div class="media mt-2">
+                                    <a href="{{ $question->user->avatar }}" class="pr-2">
+                                        <img src="{{ $question->user->avatar }}" alt="">
+                                    </a>
+                                    <div class="media-body mt-1">
+                                    <a href="{{ $question->user->url }}">{{ $question->user->name }}</a>
+                                    </div>
+                                </div>
+                            </div><!-- end of float-right -->
+                        </div><!-- end of media body -->
+                    </div><!-- end of media -->
+                </div><!-- end of card body -->
+
             </div><!-- end of card -->
         </div><!-- end of col-md-12 -->
     </div><!-- end of row -->
+
     <div class="row mt-3">
         <div class="col-md-12">
             <div class="card">
                 <div class="card-body">
+
                     <div class="card-title">
                         <h2>{{ $question->answers_count . " " . str_plural('Answer', $question->answers_count) }}</h2>
                     </div>
+
                     <hr>
+
                     @foreach ($question->answers as $answer)
                         <div class="media">
+                            <div class="d-flex flex-column vote-controls">
+                                <a title="This answer is useful" class="vote-up">
+                                    <i class="fas fa-caret-up fa-3x"></i>
+                                </a>
+                                <span class="votes-count">1230</span>
+                                <a title="This answer is not useful" class="vote-down off">
+                                        <i class="fas fa-caret-down fa-3x"></i>
+                                </a>
+                                <a title="Mark this answer as bes answer" class="vote-accepted mt-2">
+                                    <i class="fas fa-check fa-3x"></i>
+                                </a>   
+                            </div><!-- end of d flex -->
+                            
                             <div class="media-body">
                                 {!! $answer->body_html !!}
                                 <div class="float-right">
@@ -57,6 +97,7 @@
                         </div><!-- end of media -->
                         <hr>
                     @endforeach
+                    
                 </div><!-- end of card body -->
             </div><!-- end of card -->
         </div><!-- end of col-md-12 -->

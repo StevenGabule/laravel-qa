946495fe8ef92273695e57b3f1145570ae2db78e
John Doe <lucas.gabule@gmail.com>
2019-06-11 06:30:55 +0800

    done clearing the view and it's quite amazing learning

---
+++ resources/views/answers/_answer.blade.php
@@ -0,0 +1,47 @@
+<div class="media post">
+
+    @include('shared._vote', [
+        'model' => $answer
+    ])
+        
+    <div class="media-body">
+
+        {!! $answer->body_html !!}
+
+            <div class="row">
+
+                <div class="col-4">
+
+                    <div class="ml-auto">
+
+                        @can ('update', $answer)
+
+                            <a href="{{ route('questions.answers.edit', [$question->id, $answer->id]) }}" class="btn btn-sm btn-outline-info">Edit</a>
+                    
+                        @endcan
+
+                        @can ('delete', $answer)
+                            <form action="{{ route('questions.answers.destroy', [$question->id, $answer->id])}}" class="d-inline" method="post">
+                                @method('DELETE')
+                                @csrf
+                                <button type="submit" class="btn btn-sm btn-outline-danger" onclick="return confirm('Are you sure to delete this question?')">
+                                    Delete
+                                </button>
+                            </form>
+                        @endcan
+                    </div>
+                </div><!-- end of col-4 -->
+
+                <div class="col-4"></div><!-- end of col-4 -->
+                
+                <div class="col-4">
+
+                    @include('shared._author', [
+                        'model' => $answer,
+                        'label' => 'answered'
+                    ])
+
+                </div><!-- end of col-4 -->
+        </div><!-- end of row -->
+    </div><!-- end of media body -->
+</div><!-- end of media -->
--- resources/views/answers/_index.blade.php
+++ resources/views/answers/_index.blade.php
@@ -20,79 +20,7 @@
 
                 @foreach ($answers as $answer)
 
-                    <div class="media">
-
-                        <div class="d-flex flex-column vote-controls">
-
-                            <a title="This answer is useful" 
-                                class="vote-up {{ Auth::guest() ? 'off' : '' }}" 
-                                onclick="event.preventDefault();document.getElementById('up-vote-answer-{{ $answer->id }}').submit();">
-                                <i class="fas fa-caret-up fa-3x"></i>
-                            </a>
-
-                            <form id="up-vote-answer-{{ $answer->id }}" action="/answers/{{ $answer->id }}/vote" method="POST" >
-                                @csrf
-                                <input type="hidden" name="vote" value="1">
-                            </form>
-
-                            <span class="votes-count">{{ $answer->votes_count }}</span>
-                            
-                            <a class="vote-down {{ Auth::guest() ? 'off' : '' }}"
-                            onclick="event.preventDefault();document.getElementById('down-vote-answer-{{ $answer->id }}').submit();"
-                            title="This answer is not useful" >
-                                    <i class="fas fa-caret-down fa-3x"></i>
-                            </a>
-
-                            <form action="/answers/{{ $answer->id }}/vote" method="POST" id="down-vote-answer-{{ $answer->id }}">
-                                @csrf
-                                <input type="hidden" name="vote" value="-1">
-                            </form>
-
-                        </div><!-- end of d flex -->
-                        
-                        <div class="media-body">
-
-                            {!! $answer->body_html !!}
-
-                                <div class="row">
-
-                                    <div class="col-4">
-
-                                        <div class="ml-auto">
-
-                                            @can ('update', $answer)
-
-                                                <a href="{{ route('questions.answers.edit', [$question->id, $answer->id]) }}" class="btn btn-sm btn-outline-info">Edit</a>
-                                        
-                                            @endcan
-    
-                                            @can ('delete', $answer)
-                                                <form action="{{ route('questions.answers.destroy', [$question->id, $answer->id])}}" class="d-inline" method="post">
-                                                    @method('DELETE')
-                                                    @csrf
-                                                    <button type="submit" class="btn btn-sm btn-outline-danger" onclick="return confirm('Are you sure to delete this question?')">
-                                                        Delete
-                                                    </button>
-                                                </form>
-                                            @endcan
-                                        </div>
-                                    </div><!-- end of col-4 -->
-
-                                    <div class="col-4"></div><!-- end of col-4 -->
-                                    
-                                    <div class="col-4">
-
-                                        @include('shared._author', [
-                                            'model' => $answer,
-                                            'label' => 'answered'
-                                        ])
-
-                                   </div><!-- end of col-4 -->
-
-                                </div><!-- end of row -->
-                            </div><!-- end of media body -->
-                        </div><!-- end of media -->
-                    <hr>
+                 @include('answers._answer', ['answer' => $answer])
                 @endforeach
                 
             </div><!-- end of card body -->
@@ -101,3 +29,4 @@
 </div><!-- end of row -->
 
 @endif
+
